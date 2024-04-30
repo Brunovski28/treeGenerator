9159184fb170c02384951ec33783a4b340dab3ff
Brunovski28 <bruno.procamilo@gmail.com>
2024-04-30 19:04:56 -0300

    Commit final

--- js/tree-generator.js
+++ js/tree-generator.js
@@ -12,22 +12,25 @@ AFRAME.registerComponent('tree-generator', {
         var sceneEl = this.el;
         var treeTemplate = document.querySelector('#tree-template');
 
+        var backupPosX = this.data.posX;
+
         for (var i = 0; i < this.data.columns; i++) {
             for (var j = 0; j < this.data.quantityTree; j++){
 
-            var scale = Math.random() * 0.5 + 0.5;
-            
-            this.data.posX += 5 + this.data.posAltX;
+                var scale = Math.random() * 0.5 + 0.5;
+                
+                this.data.posX += 5 + this.data.posAltX; // Cálculo para distanciar as árvores das outras na linha X
 
-            var tree = treeTemplate.cloneNode(true);
-            tree.setAttribute('position', { x: this.data.posX + (Math.random() * 3), y: 0, z: this.data.posZ + (Math.random() * 3)});
-            tree.setAttribute('scale', { x: scale, y: scale, z: scale });
+                var tree = treeTemplate.cloneNode(true);
+                tree.setAttribute('position', { x: this.data.posX + (Math.random() * 3), y: 0, z: this.data.posZ + (Math.random() * 3)});
+                tree.setAttribute('scale', { x: scale, y: scale, z: scale });
 
-            sceneEl.appendChild(tree);
+                sceneEl.appendChild(tree);
+
             }
 
-            this.data.posX = -55 // Tá fixo aqui, vai ter um problema
-            this.data.posZ += 10 + this.data.posAltY        
+            this.data.posX = backupPosX // Cálculo para o reset da linha x, voltando para a linha inicial
+            this.data.posZ += 10 + this.data.posAltY // Cálculo para pular na próxima coluna   
             
         }
     }
